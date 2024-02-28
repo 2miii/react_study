a1e48336a394c140002a7c36383c5865b2f514a6
Miyeon Lee <2miii@naver.com>
2024-02-29 00:21:26 +0900

    chapter 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';  //index.css를 가져오는 것~
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
