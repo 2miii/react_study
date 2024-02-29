13e9059ed48b3c5e60467a784ea5af852a824440
Miyeon Lee <2miii@naver.com>
2024-02-29 22:54:40 +0900

    props

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,12 +4,13 @@ import ExpenseItem from './ExpenseItem';
 // rcc
 export default class ExpenseList extends Component {
   render() {
+    
+    console.log('@@@',this.props.initialExpenses)//props.확인
     return (
      // div 대신 react.Fragment사용하여 감싸줄수있다. <></>로 사용해도 됨
       <React.Fragment> 
           <ul className='list'>
                <ExpenseItem/>
-               {/* <ExpenseItem/> */}
           </ul>
           <button className='btn'>
                     목록 지우기
