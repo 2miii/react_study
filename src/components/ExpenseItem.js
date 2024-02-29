c898000ba7e01531c02ec4831a0fdb6ea265547c
Miyeon Lee <2miii@naver.com>
2024-02-29 22:38:07 +0900

    reactIcon

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './ExpenseItem.css';
+import { MdEdit,MdDelete } from "react-icons/md";
 
 export default class ExpenseItem extends Component {
   render() {
@@ -10,15 +11,18 @@ export default class ExpenseItem extends Component {
             <span className='expense'>빵</span>
             <span className='amount'>100원</span>
           </div>
-        </li>
-        <div>
+          <div>
           <button className='edit-btn'>
-            수정
+            {/* 수정 */}
+            <MdEdit />
           </button>
           <button  className='clear-btn'>
-            삭제
+            {/* 삭제 */}
+            <MdDelete />
           </button>
         </div>
+        </li>
+        
       </>
     )
   }
